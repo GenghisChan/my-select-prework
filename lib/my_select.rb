2d983a8eb6dba72c4cefc60d08e29e7d383016af
Christofer Chan <devchumbing@gmail.com>
2018-06-18 23:13:28 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(array)
   i = 0
   new_a = []
   while i < array.length
-    if yield(array[i])
-      new_a << array[i]
+      yield(array[i])
+      new_a.push(array[i])
     end
    i += 1
  end
